3f26e76c8c3ed1082718d73e1e957473bbf2bcc6
Abylay Dauletkhan <dabylai@bk.ru>
2023-11-13 22:55:10 +0600

    promise

---
+++ async-demo/promise.js
@@ -0,0 +1,10 @@
+const p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        reject(new Error('message'));
+    }, 2000);
+    resolve(arodca());
+});
+
+p.then((result) => console.log('Result:', result)).catch((err) =>
+    console.log('Error', err.message)
+);
