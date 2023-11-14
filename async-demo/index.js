44653eb24527820bbeb0b198a196f0b0c1d3668a
Abylay Dauletkhan <dabylai@bk.ru>
2023-11-14 14:17:01 +0600

    promises

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,35 +1,48 @@
 console.log('Start');
 
-getUser(1, displayUser);
+getUser(1)
+    .then(displayUser)
+    .then(displayRepos)
+    .then(displayCommits)
+    .catch((error) => console.log('Error:', error.message));
 
 console.log('End');
 
 function displayUser(user) {
-    console.log(user);
-    getRepositories(user.gitHubUsername, displayRepos);
+    return new Promise((resolve, reject) => {
+        console.log(user);
+        resolve(getRepositories(user.gitHubUsername));
+    });
 }
 
-function displayRepos(repos, displayCommits) {
-    console.log(repos);
-    const commits = ['async-demo', 'express-demo', 'vidly'];
-    console.log('Getting commits from:', repos[0]);
-    displayCommits(commits);
+function displayRepos(repos) {
+    return new Promise((resolve, reject) => {
+        console.log(repos);
+        setTimeout(() => {
+            console.log('Getting commits from:', repos[0]);
+            resolve(['async-demo', 'express-demo', 'vidly']);
+        }, 2000);
+    });
 }
 
 function displayCommits(commits) {
     console.log(commits);
 }
 
-function getUser(id, cb) {
-    setTimeout(() => {
-        console.log('Reading a user from a database...');
-        cb({ id: id, gitHubUsername: 'mosh' });
-    }, 2000);
+function getUser(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Reading a user from a database...');
+            resolve({ id: id, gitHubUsername: 'mosh' });
+        }, 2000);
+    });
 }
 
-function getRepositories(username, displayRepos) {
-    setTimeout(() => {
-        console.log('Getting the repositories of the user:', username);
-        displayRepos(['repo1', 'repo2', 'repo3'], displayCommits);
-    }, 2000);
+function getRepositories(username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Getting the repositories of the user:', username);
+            resolve(['repo1', 'repo2', 'repo3']);
+        }, 2000);
+    });
 }
